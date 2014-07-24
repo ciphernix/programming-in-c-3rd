46fa2f4c14e6588d50010eee557d19ca0f1e5b60
ciphernix <ciphernix@gmail.com>
2014-07-23 21:49:00 -0400

    Fixed exercised 2b/3

--- chapter11/c1102b.c
+++ chapter11/c1102b.c
@@ -16,7 +16,7 @@
  struct entry *root;  // use to always points to the first node
 
 //Function to insert a new entry after the specified node
- void insertEntry(struct entry *node; struct entry *newEntry )
+ void insertEntry(struct entry *node, struct entry *newEntry )
  {
  	struct entry *temp;
  	
@@ -48,7 +48,6 @@ int main (void)
 {
 	struct entry  node0, node1, node2, node3, node4;
 	root = &node0;
-	struct entry *listPtr = root;
 
 	node0.value = 0;
 	node0.next = &node1;
@@ -59,17 +58,17 @@ int main (void)
 	node3.value = 3;
 	node4.value = 4;
 
-	printList (listPtr);
+	printList (root);
 	
 	printf("\nInserting value %i after value %i\n", node3.value, node2.value);
 	insertEntry(&node2, &node3);
 
-	printList(listPtr);
+	printList(root);
 
 	printf("\nInserting value %i at the beginning\n", node4.value);
 	insertEntry(&node4,(struct entry *) 0 );
 
-	printList(listPtr);
+	printList(root);
 	
 	return 0;
 }
