79cae5c77c0602021da114dc6009f38f8ffabdf1
ciphernix <ciphernix@gmail.com>
2014-07-26 11:10:17 -0400

    c11 e 7

--- chapter11/c1107.c
+++ chapter11/c1107.c
@@ -62,3 +62,25 @@ void sort (int *array, int *n)
 		}
 	}
 }
+
+int main (void) 
+{
+        int i;
+        int array[16] = {34, -5, 6, 0, 12, 100, 56, 22, 
+                                        44, -3, -9, 12, 17, 22, 6, 11 }; 
+        void sort (int *a, int *n);
+        
+        printf ("The array before the sort:\n");
+        for(i = 0; i < 16; ++i) 
+                printf ("%i ", array[i]);
+        
+        sort (int array, int &i);
+        
+        printf ("\n\nThe array after the sort:\n");
+        for(i = 0; i < 16; ++i) 
+                printf ("%i ", array[i]);
+                        
+        printf ("\n");          
+                                
+        return 0;               
+}
