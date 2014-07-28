287eb5c2586b8deacb988b42b43913412848f262
ciphernix <ciphernix@gmail.com>
2014-07-28 07:15:02 -0400

    Scrippted commit

---
+++ chapter11/c1111.c
@@ -0,0 +1,58 @@
+/*
+ *Chapter 11 exercise 11:
+ *given the the def of a date structure as define in this chapter
+ * struct date
+ * {
+ *  	int month;
+ *		int day;
+ *		int year;
+ * };
+ * write a function called dateUpdate that takes a pointer to a date
+ * structure as its arguement and thst updates the structue to the following
+ * day.
+ */
+ 
+ #include <stdio.h>
+ #include <stdbool.h>
+ 
+ struct date
+ {
+ 	int month;
+ 	int day;
+ 	int year;
+ }
+
+//function to determine if a year is leap 
+ bool isLeap(int year)
+ {
+ 	if ((year % 4 == 0 && year % 100 != 0) ||
+ 			year % 400 == 0)
+ 			return true;
+ 	else 
+ 		return false;
+ }
+ 
+ //function to determine the numbers of days in a montg 
+ int daysInMonth (struct date *newDate)
+ {
+ 	int month[12] = { 0, 31, 28, 31, 30, 31, 30, 31
+ 						31, 30, 31, 30, 31};
+ 	int i = newDate->month;
+ 	int year == newDate->year;
+ 	
+ 	if (i == 2 && isLeap(year))
+ 		month[i]++;
+ 	
+ 	return month[i];
+ }
+ 
+ 
+ void dateUpdate (struct date *newDate)
+ {
+ 	if (newDate->day < daysInMonth(newDate) )
+ 		newDate->day++;
+ 	else {
+ 		newDate->day = 1;
+ 		if (month 
+ 	}
+ }
