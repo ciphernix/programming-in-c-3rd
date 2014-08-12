220aa3b39f03b17f8e0539afaba86b2cab1b274e
ciphernix <ciphernix@gmail.com>
2014-08-12 18:30:03 -0400

    Scrippted commit

--- chapter14/c1402.c
+++ chapter14/c1402.c
@@ -7,18 +7,75 @@
  */
 
 #include <stdio.h>
-enum month { january = 1, february, march, april, may, june, / 
+enum month { january = 1, february, march, april, may, june, \ 
 	july, august, september, october, november, december };
 
-char * monthName(enum month)
+char *monthName(enum month amonth)
 {
 	char *res;
 	
-	switch (month) {
+	switch (amonth) {
 		case january:
 			res = "January";
 			break;
 		case february:
-			res = "February" 
-		
-	}
+			res = "February";
+			break;
+		case march:
+			res = "March";
+			break;
+		case april:
+			res = "April";
+			break;
+		case may:
+			res = "May";
+			break;
+		case june:
+			res = "June";
+			break;
+		case july:
+			res = "July";
+			break;
+		case august:
+			res = "August";
+			break;
+		case september:
+			res = "September";
+			break;
+		case october:
+			res = "October";
+			break;
+		case november:
+			res = "November";
+			break;
+		case december:
+			res = "December";		
+			break;
+		default:
+			res = "Bad month";
+			break;
+		}
+		return res;	
+}
+
+//same solution but using an array
+char *monthNameArray(enum month amonth)
+{
+	char *months[] = {"no month", "January", "February", "March", \
+		"April", "May", "June", "July", "August", "September", "October", \
+		"November", "December"};
+	
+	return months[amonth];
+}
+
+int main (void)
+{
+	enum month amonth;
+	printf("Enter digit month: ");
+	scanf("%i", &amonth);
+	
+	printf("Using monthName %s\n",monthName(amonth));
+	printf("Using monthNamArray %s\n",monthNameArray(amonth));
+	
+	return 0;
+}
