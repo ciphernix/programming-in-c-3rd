414c7bc3bba1fa2df1fdc203f73431377a3d044b
ciphernix <ciphernix@gmail.com>
2014-08-23 19:21:57 -0400

    Testing scanf

--- chapter16/scanf_test.c
+++ chapter16/scanf_test.c
@@ -3,12 +3,17 @@
 int main(void)
 {
 	char input[81];
+	char input2[81];
 	int n;
+	int i;
 
-	printf	(" : ");
-	scanf("%80[^q\n]%n", input, &n);
+	printf(" : ");
+	scanf("%80[^\n]%n", input, &n);
 	printf("input = %s\nn = %i\n", input, n);
 
+	printf(" : ");
+	scanf("%80[^q]%n", input2, &i);
+	printf("input2 = %s\ni = %i\n", input2, i);
 	return 0;
 }
 
